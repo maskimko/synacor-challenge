e2dfc98a0a8ab2cfe1b4c53c568b957403fd1fc9
Maksym Shkolnyi <maskimko@ukr.net>
2026-01-08 16:34:11 +0100

    format

--- var1/solution/synacor_challenge_v1/src/main.rs
+++ var1/solution/synacor_challenge_v1/src/main.rs
@@ -1,20 +1,23 @@
-use synacor_challenge_v1::*;
-use synacor_challenge_v1::config::*;
 use log::warn;
+use synacor_challenge_v1::config::*;
+use synacor_challenge_v1::*;
 
 fn main() {
     println!("Welcome to SYNACOR challenge!");
-    let conf: Configuration  = match parse_args() {
-        Ok(c) => c, 
+    let conf: Configuration = match parse_args() {
+        Ok(c) => c,
         Err(e) => {
-    
-    let c = Configuration::default();
+            let c = Configuration::default();
 
-warn!("Failed to parse configuration. Fallback to default value {:?}", c );
-    c },
+            warn!(
+                "Failed to parse configuration. Fallback to default value {:?}",
+                c
+            );
+            c
+        }
     };
-    match run(conf) { 
+    match run(conf) {
         Ok(()) => println!("Challenge program finished successfully"),
         Err(e) => eprintln!("Error: {}", e),
-};
+    };
 }
