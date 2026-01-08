8b1161ddd73332d01a7dea6a79eafc982aa7bf4d
Maksym Shkolnyi <maskimko@ukr.net>
2026-01-08 21:43:07 +0100

    add auxiliary methods

--- var1/solution/synacor_challenge_v1/src/lib.rs
+++ var1/solution/synacor_challenge_v1/src/lib.rs
@@ -1,9 +1,11 @@
-use log::{debug, warn};
+use log::{debug, error, trace};
 use std::error::Error;
+use std::fmt::{self, Formatter};
 
 pub mod config;
 
-const MAX: u16 = 32768; // The same as 1 << 15
+//const MAX: u16 = 32768; // The same as 1 << 15
+const MAX: u16 = 1<<15; 
 struct VM {
     halt: bool,
     memory: [u8; 1 << 15],
@@ -11,6 +13,7 @@ struct VM {
     stack: Vec<u16>,
     // - all numbers are unsigned integers 0..32767 (15-bit)
     // - all math is modulo 32768; 32758 + 15 => 5
+    current_address: Address, // internal execution pointer
 }
 
 /*
@@ -25,6 +28,13 @@ struct VM {
 
 // Points to the u8 data value in the memory array
 type Ptr = u16;
+
+impl From<Address> for Ptr {
+    fn from(a : Address) -> Self {
+        (a.0 * 2) as Ptr
+    }
+}
+
 struct Address(u16);
 
 impl Address {
@@ -38,6 +48,19 @@ impl Address {
     fn from_ptr(value: u16) -> Self {
         (value as Ptr).into()
     }
+
+    fn next(&self) -> Self {
+        self.add(1)
+    }
+    fn add(&self, n: u16) -> Self {
+        Address::new(self.0 + n)
+    }
+}
+
+impl fmt::Display for Address {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "addr[{}]",self.0)
+    }
 }
 
 impl From<Ptr> for Address {
@@ -45,12 +68,99 @@ impl From<Ptr> for Address {
     // In other words address points into 2 consequtive u8 values in the memory
     fn from(p: Ptr) -> Self {
         if p % 2 == 1 {
-            warn!(
-                "provided pointer {} must be even! the value will be floored to the lesser one",
-                p
-            );
+            error!( "provided pointer {} must be even! the value will be floored to the lesser one", p);
+            // For a moment just to spot the anomaly
+            panic!(                "provided pointer {} must be even! the value will be floored to the lesser one", p);
         }
         Address::new(p / 2)
+    }
+}
+
+
+enum Data {
+    LiteralValue(u16),
+    Register(usize)
+}
+    fn compose_value(byte_pair: (u8, u8)) -> u16 {
+    // - all math is modulo 32768; 32758 + 15 => 5
+    // - each number is stored as a 16-bit little-endian pair (low byte, high byte)
+    let lb :u16= byte_pair.0 as u16;
+    let hb :u16= (byte_pair.1 as u16) << 8;
+    let value = (hb + lb) % MAX;
+    trace!("compose value {} from bytes {:?}", value, byte_pair)    ;
+    value
+    }
+    fn decompose_value(value: u16) -> (u8, u8) {
+    // - all math is modulo 32768; 32758 + 15 => 5
+    // - each number is stored as a 16-bit little-endian pair (low byte, high byte)
+    let lb: u8 = (value % 8) as u8;
+    let hb : u8= (value >> 8) as u8;
+    let byte_pair = (lb, hb);
+    trace!("decompose bytes {:?} from value {} ",byte_pair , value)    ;
+     return byte_pair;
+    }
+
+impl VM {
+
+    fn get_value_from_addr(&self, addr: Address) -> u16 {
+        trace!("getting value from address {}", addr);
+        let ptr = addr.into();
+        let lb = self.get_byte_value_from_ptr(ptr);
+        let hb = self.get_byte_value_from_ptr(ptr+1);
+        compose_value((lb, hb))
+    }
+    fn get_byte_value_from_ptr(&self, ptr: Ptr) -> u8 {
+        let b  = self.memory[ptr as usize];
+        trace!("fetched {} form memory pointer {} ", b, ptr);
+        b
+    }
+    fn get_data_from_raw_value(&self, v: u16) -> Data {
+        let data = match v {
+            val if v < MAX => { 
+                trace!("packing literal value {}", v);
+                Data::LiteralValue(val)
+            },
+            r if r % MAX < 8 => { 
+                trace!("packing register number {}", v);
+                Data::Register((r%MAX) as usize)
+            },
+            _ => panic!("values bigger than 32776 are invalid"),
+        };
+        data
+    }
+
+    fn get_data(&self, v: u16) -> u16 {
+        match self.get_data_from_raw_value(v) {
+            Data::LiteralValue(lv) => lv,
+            Data::Register(r) => self.get_from_register(r),
+        }
+    }
+
+    fn get_from_register(&self, register: usize) -> u16 {
+        if register >=8 {
+            panic!("invalid register value {} There is 8 resisters only.", register);
+        }
+        let v = self.registers[register];
+        trace!("getting value {} from register {}", v, register);
+        v
+    }
+
+    fn step(&mut self) {
+       self.current_address = self.current_address.next(); 
+    }
+    fn step_n(&mut self, n: u16) {
+       self.current_address = self.current_address.add(n) ;
+    }
+    fn main_loop(&self) -> Result<u64, Box<dyn Error>> {
+
+        let mut cycles : u64= 0;
+
+        loop {
+            cycles +=1 ;
+            unimplemented!("main loop is not implemented yet");
+        }
+
+        Ok(cycles)
     }
 }
 
